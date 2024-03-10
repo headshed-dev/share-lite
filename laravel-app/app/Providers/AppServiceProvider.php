a04b0b7cdd9e1a62477146a07f66d087acde1184
jon brookes <jon@vmi1596446.contaboserver.net>
2024-03-10 14:56:00 +0000

    fix: compose and https environment

--- laravel-app/app/Providers/AppServiceProvider.php
+++ laravel-app/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+	if (app()->environment('local')) 
+	{
+        	// For local development, force HTTP
+        	URL::forceScheme('http');
+	} 
+	
+	else {
+        	// For other environments, force HTTPS
+        	URL::forceScheme('https');
+    	}
     }
 }
